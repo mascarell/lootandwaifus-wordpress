6c6d2db6adbd1c464dffc20de909da7fc958ce38
Mascarell <echo.mascarell@gmail.com>
2023-02-21 13:49:10 +0100

    inline characters

---
+++ template-parts/character-inline.php
@@ -0,0 +1,17 @@
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php
+		$tags = get_the_tags(); // get an array of tags associated with the post
+		if ( $tags ) {
+			$first_tag = reset( $tags ); // get the first tag in the array
+		}
+	?>
+
+	<div class="post character covering container" data-character-name="<?php the_title(); ?>">
+		<div class="lozad <?php echo $first_tag->slug; ?>" data-background-image="<?php echo the_post_thumbnail_url(); ?>"></div>
+		<a href="<?php the_permalink(); ?>" class="margin">
+			<div class="content">
+				<h2><?php the_title(); ?></h2>
+			</div>
+		</a>
+	</div>
+<?php endwhile; // end of the loop. ?>
