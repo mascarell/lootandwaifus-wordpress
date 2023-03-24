0b1570d43d128f05c36c6e6620b3ead7288dbba1
Mascarell <echo.mascarell@gmail.com>
2023-03-24 13:49:46 +0100

    updated icon

--- template-parts/guide.php
+++ template-parts/guide.php
@@ -1,4 +1,12 @@
 <div class="post guide covering container">
+		<?php
+	$last_modified = strtotime(get_the_modified_date('Y-m-d')); // get the last modified date of the post
+	$one_week_ago = strtotime('-1 week'); // get the timestamp of one week ago
+
+	if ($last_modified > $one_week_ago) {
+		?> <span class="updated">updated</span> <?php
+	}
+	?>
 	<a href="<?php the_permalink(); ?>" class="margin">
 		<div class="thumbnail" href="<?php the_permalink(); ?>">
 			<div class="img lozad" data-background-image="<?php echo the_post_thumbnail_url(); ?>"></div>
