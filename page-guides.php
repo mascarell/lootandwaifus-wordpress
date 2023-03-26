516752434701264ec286aaf987733fc098a2c2a2
Mascarell <echo.mascarell@gmail.com>
2023-03-26 21:07:17 +0200

    updated timezone

--- page-guides.php
+++ page-guides.php
@@ -55,7 +55,7 @@ $category_id = $categories[0]->name;
 			$currentHour = date('H');
 	
 			// Determine which text to display based on time and day of year
-			if ($currentHour >= 21) {
+			if ($currentHour >= 22) {
 					// If it's past 9pm, show the text for the next day
 					$dayOfYear = date('z') + 2; // Add 2 to skip the current day and go to the next day
 			} else {
