8acb121a5c3f46f4bf5ec40e7ee16466fb337b4b
Mascarell <echo.mascarell@gmail.com>
2023-05-21 14:02:13 +0200

    news on homepage

--- index.php
+++ index.php
@@ -26,6 +26,34 @@ get_header();
 	</a>
 </div>
 
+<div class="double">
+	<div class="news">
+		<div class="posts animated">
+			<?php
+					$args = array(
+							'posts_per_page' => 8,
+							'post_type'      => 'post',
+							'paged'          => get_query_var( 'paged' ),
+					);
+					$wp_query = new WP_Query( $args );
+	
+					if( have_posts() ):
+							while ( $wp_query->have_posts() ) : $wp_query->the_post();
+	
+							get_template_part('template-parts/content-news');
+							
+							endwhile;            
+					endif;
+							
+			wp_reset_query(); ?>
+		</div>
+
+		<?php get_template_part('template-parts/pagination'); ?>
+	</div>
+
+	<?php get_template_part('template-parts/sidebar'); ?>
+</div>
+
 <?php
 
 get_footer();
