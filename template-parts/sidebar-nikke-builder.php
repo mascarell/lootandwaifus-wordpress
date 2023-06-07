59a9159a3d9a338460cdcc70edab7412cc8f5698
Mascarell <echo.mascarell@gmail.com>
2023-06-07 08:12:30 +0200

    fixed class

--- template-parts/sidebar-nikke-builder.php
+++ template-parts/sidebar-nikke-builder.php
@@ -13,7 +13,7 @@
 	<div class="container">
 		<h3>Sponsor us</h3>
 
-		<p>Want to sponsor the website and YouTube videos? Send us an email to <a href="mailto:contact@lootandwaifus.com">contact@lootandwaifus.com</a>.</p>
+		<p>Want to sponsor the website and/or our YouTube videos? You can say hello at <a class="email" href="mailto:contact@lootandwaifus.com">contact@lootandwaifus.com</a> and we'll get back to you.</p>
 	</div>
 
 	<div class="container">
