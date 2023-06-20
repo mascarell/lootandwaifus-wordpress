ba9d906096120dadbd99f44fcaccdb8f918a33dd
Mascarell <echo.mascarell@gmail.com>
2023-06-20 19:20:11 +0200

    ttwitch embed

--- template-parts/sidebar-nikke-builder.php
+++ template-parts/sidebar-nikke-builder.php
@@ -17,6 +17,25 @@
 	</div>
 
 	<div class="container">
+		<!-- Add a placeholder for the Twitch embed -->
+		<div id="twitch-embed"></div>
+
+		<!-- Load the Twitch embed JavaScript file -->
+		<script src="https://embed.twitch.tv/embed/v1.js"></script>
+
+		<!-- Create a Twitch.Embed object that will render within the "twitch-embed" element -->
+		<script type="text/javascript">
+			new Twitch.Embed("twitch-embed", {
+				width: '100%',
+				height: 480,
+				channel: "sefhi_922",
+				// Only needed if this page is going to be embedded on other websites
+				parent: ["embed.example.com", "othersite.example.com"]
+			});
+		</script>
+	</div>
+
+	<div class="container">
 		<a class="patreon" href="https://patreon.com/lootandwaifus" target="_blank" rel="noopener noreferrer">
 			<img src="<?php echo get_theme_file_uri('images/patreon.png'); ?>" loading="lazy" alt="support us on patreon">
 		</a>
