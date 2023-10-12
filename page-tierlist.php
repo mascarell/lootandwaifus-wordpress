3308dc40f93919a53e7e6e2d212b741f41b58b89
Sefhi <echo.mascarell@gmail.com>
2023-10-12 16:43:59 +0200

    filters for tier lists

--- page-tierlist.php
+++ page-tierlist.php
@@ -17,12 +17,18 @@ $category_id = $categories[0]->name;
     
           <?php the_content(); ?>
 
+          <div class="character-filters">
+            <input type="search" id="search" name="search" placeholder="Filter characters" autocomplete="off">
+          </div>
+          
+          <?php get_template_part('template-parts/database-filters'); ?>
+
           <div class="tier-list">
             <!-- TIER 0 UNITS -->
             <div class="row">
               <p class="tier tier-0">Tier 0</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
@@ -43,7 +49,7 @@ $category_id = $categories[0]->name;
             <div class="row">
               <p class="tier tier-1">Tier 0.5</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
@@ -64,7 +70,7 @@ $category_id = $categories[0]->name;
             <div class="row">
               <p class="tier tier-2">Tier 1</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
@@ -85,7 +91,7 @@ $category_id = $categories[0]->name;
             <div class="row">
               <p class="tier tier-3">Tier 1.5</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
@@ -106,7 +112,7 @@ $category_id = $categories[0]->name;
             <div class="row">
               <p class="tier tier-4">Tier 2</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
@@ -127,7 +133,7 @@ $category_id = $categories[0]->name;
             <div class="row">
               <p class="tier tier-5">Tier 3</p>
 
-              <div class="characters">
+              <div class="characters filtered">
                 <?php
                   $args = array(
                     'post_type' => 'characters', 
