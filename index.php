6f1f3b1c1ee2441fcdeaa55420101c9a4d949092
Sefhi <echo.mascarell@gmail.com>
2023-10-17 07:07:24 +0200

    updated index style

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
 ?>
 
 <!-- Latest video section with WP customizer -->
-<div class="latest-video animated">
+<!-- <div class="latest-video animated">
 	<div>
 		<?php
 			$custom_link = get_theme_mod( 'custom_link' );
@@ -23,6 +23,34 @@ get_header();
 			if ( $custom_image ) { echo '<img src="' . esc_url( $custom_image ) . '">'; }
 		?>
 	</div>
+</div> -->
+
+<!-- Hero section -->
+<div class="hero animated">
+  <h1>Your one-stop shop for in depth gacha games tier lists and guides</h1>
+  <p>Loot & Waifus is a community driven wiki for the games we love and care about. Click down below on your favorite game to get started.</p>
+</div>
+
+<!-- Games that we cover -->
+<div class="games container animated">
+	<a href="<?php echo home_url(); ?>/nikke" class="game nikke covering">
+		<div>
+			<p>Goddess of Victory: Nikke</p>
+		</div>
+	</a>
+	<a href="<?php echo home_url(); ?>/reverse-1999" class="game reverse covering">
+		<div>
+			<p>Reverse: 1999</p>
+		</div>
+	</a>
+	<a class="game">
+		<div>
+		</div>
+	</a>
+	<a class="game">
+		<div>
+		</div>
+	</a>
 </div>
 
 <div class="double">
