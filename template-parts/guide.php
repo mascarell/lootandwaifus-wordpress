03deb5d24fc0ea34bd83a2d3cf18cd1786330f41
Sefhi <echo.mascarell@gmail.com>
2023-10-25 17:27:50 +0200

    guides withouth thumbnail

--- template-parts/guide.php
+++ template-parts/guide.php
@@ -1,16 +1,22 @@
 <div class="post guide covering container">
-		<?php
-	$last_modified = strtotime(get_the_modified_date('Y-m-d')); // get the last modified date of the post
-	$one_week_ago = strtotime('-6 days'); // get the timestamp of 4 days ago
+  <?php
+    $last_modified = strtotime(get_the_modified_date('Y-m-d')); // get the last modified date of the post
+    $one_week_ago = strtotime('-6 days'); // get the timestamp of 4 days ago
 
-	if ($last_modified > $one_week_ago) {
-		?> <span class="updated">updated</span> <?php
-	}
+    if ($last_modified > $one_week_ago) {
+      ?> <span class="updated">updated</span> <?php
+    }
 	?>
 	<a href="<?php the_permalink(); ?>" class="margin">
-		<div class="thumbnail" href="<?php the_permalink(); ?>">
-			<div class="img lozad" data-background-image="<?php echo the_post_thumbnail_url(); ?>"></div>
-		</div>
+    <?php
+      if (has_post_thumbnail()) {
+        ?> 
+          <div class="thumbnail" href="<?php the_permalink(); ?>">
+            <div class="img lozad" data-background-image="<?php echo the_post_thumbnail_url(); ?>"></div>
+          </div> 
+        <?php
+      }
+    ?>
 		<div class="content">
 			<h2><?php the_title(); ?></h2>
 		</div>
