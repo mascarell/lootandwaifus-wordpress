41979df338cf356bd4429b58363bf5cfde7302d5
Sefhi <echo.mascarell@gmail.com>
2023-10-30 16:18:48 +0100

    removed solo raid from main guide page

--- page-guides.php
+++ page-guides.php
@@ -216,25 +216,6 @@ if($category_id == 'Nikke') {
 					?> </div> <?php
 			endif; 
 		?>
-
-    <?php
-			$query = new WP_Query( array(
-					'post_type'      => 'guides',
-					'posts_per_page' => -1,
-					'no_found_rows'  => true,
-					'tag'            => 'solo-raid',
-					'category_name'  => $category_id
-			) );
-			if ( $query->have_posts() ) :
-					?> <h2 class="guides-title">Solo Raid ➜</h2><div class="guides"> <?php
-					while ( $query->have_posts() ) : $query->the_post();
-
-							get_template_part('template-parts/guide');
-
-					endwhile; wp_reset_postdata();
-					?> </div> <?php
-			endif; 
-		?>
 		
 		<?php
 			$query = new WP_Query( array(
