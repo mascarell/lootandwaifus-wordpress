96c090df2701b25b8c2c7b4ea170963b98477595
Sefhi <echo.mascarell@gmail.com>
2023-10-31 02:16:45 +0100

    added latest events to guides page

--- page-guides.php
+++ page-guides.php
@@ -160,6 +160,25 @@ if($category_id == 'Nikke') {
 <!-- all the guides -->
 <div class="double guides-wrapper animated">
 	<div class="news">
+    <?php
+			$query = new WP_Query( array(
+					'post_type'      => 'guides',
+					'posts_per_page' => 3,
+					'no_found_rows'  => true,
+					'tag'            => '2023',
+					'category_name'  => $category_id
+			) );
+			if ( $query->have_posts() ) :
+					?> <h2 class="guides-title">Latest events ➜</h2><div class="guides"> <?php
+					while ( $query->have_posts() ) : $query->the_post();
+
+							get_template_part('template-parts/guide');
+
+					endwhile; wp_reset_postdata();
+					?> </div> <?php
+			endif; 
+		?>
+    
 		<?php
 			$query = new WP_Query( array(
 					'post_type'      => 'guides',
