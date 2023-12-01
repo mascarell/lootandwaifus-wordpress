35ebf5fe880065ab17476bcac630c191883516ee
Sefhi <echo.mascarell@gmail.com>
2023-12-01 18:52:05 +0100

    adding new games to landing page

--- index.php
+++ index.php
@@ -38,14 +38,31 @@ get_header();
 			<p>Goddess of Victory: Nikke</p>
 		</div>
 	</a>
-	<a class="game overlord covering">
-		<div>
-			<p>Overlord: King of Nazarick</p>
-		</div>
-	</a>
-	<a href="<?php echo home_url(); ?>/reverse-1999" class="game reverse covering">
+  <a href="<?php echo home_url(); ?>/reverse-1999" class="game reverse covering">
 		<div>
 			<p>Reverse: 1999</p>
+		</div>
+	</a>
+	<a class="game overlord covering">
+		<div>
+			<p>Overlord: King of Nazarick (soon)</p>
+		</div>
+	</a>
+	<a class="game browndust covering">
+		<div>
+			<p>Brown Dust 2 (soon)</p>
+		</div>
+	</a>
+	<a class="game">
+		<div>
+		</div>
+	</a>
+	<a class="game">
+		<div>
+		</div>
+	</a>
+	<a class="game">
+		<div>
 		</div>
 	</a>
 	<a class="game">
