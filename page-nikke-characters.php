54880a536a74f22d32ee60e97f951f222bc47450
Sefhi <echo.mascarell@gmail.com>
2023-12-06 15:52:55 +0100

    ordering by rarity fixed

--- page-nikke-characters.php
+++ page-nikke-characters.php
@@ -7,6 +7,32 @@ get_header();
 $categories = get_the_category();
 $category_id = $categories[0]->name;
 
+function showCharactersByRarity($tag_slug, $category_id) {
+  $query = new WP_Query( array(
+      'post_type'      => 'characters',
+      'orderby'        => 'title',
+      'order'          => 'ASC',
+      'posts_per_page' => -1,
+      'no_found_rows'  => true,
+      'category_name'  => $category_id,
+      'tax_query' => array(
+          array(
+              'taxonomy' => 'post_tag',  // Use the taxonomy name for tags
+              'field'    => 'slug',      // Use 'slug' or 'term_id' as per your requirement
+              'terms'    => $tag_slug,  // Replace with the slug of your desired tag
+          ),
+      ),
+  ) );
+  if ( $query->have_posts() ) :
+
+      while ( $query->have_posts() ) : $query->the_post();
+
+        get_template_part('template-parts/character-nomargin');
+
+      endwhile; wp_reset_postdata();
+  endif;
+}
+
 ?>
 
 <div class="double characters-wrapper animated">
@@ -21,29 +47,10 @@ $category_id = $categories[0]->name;
 			<?php
         // We have a different ordering method for reverse 1999 characters
         if($category_id == 'Reverse: 1999') {
-          $query = new WP_Query( array(
-              'post_type'      => 'characters',
-              'posts_per_page' => -1,
-              'no_found_rows'  => true,
-              'category_name'  => $category_id,
-              'tax_query'      => array(
-                  'relation' => 'AND', // Specify that posts must have all specified tags
-                  array(
-                      'taxonomy' => 'post_tag',
-                      'field'    => 'slug',
-                      'terms'    => array( 'ur', 'ssr', 'sr', 'r' ),
-                  ),
-              ),
-              'orderby'        => 'terms_order', // Order by the order of tags specified
-              'order'          => 'DESC',
-          ) );
-
-          if ( $query->have_posts() ) :
-              while ( $query->have_posts() ) : $query->the_post();
-                  get_template_part('template-parts/character-nomargin');
-              endwhile;
-              wp_reset_postdata();
-          endif;
+          showCharactersByRarity('ur', $category_id);
+          showCharactersByRarity('ssr', $category_id);
+          showCharactersByRarity('sr', $category_id);
+          showCharactersByRarity('r', $category_id);
         } else {
           $query = new WP_Query( array(
               'post_type'      => 'characters',
