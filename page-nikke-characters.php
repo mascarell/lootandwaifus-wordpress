8060e507dc766dfa3a84c64a8d581a4c521855c6
Sefhi <echo.mascarell@gmail.com>
2023-12-06 15:10:42 +0100

    reverse characters change ordering

--- page-nikke-characters.php
+++ page-nikke-characters.php
@@ -19,22 +19,49 @@ $category_id = $categories[0]->name;
 
 		<div class="characters filtered">
 			<?php
-				$query = new WP_Query( array(
-						'post_type'      => 'characters',
-						'orderby'        => 'title',
-						'order'          => 'ASC',
-						'posts_per_page' => -1,
-						'no_found_rows'  => true,
-						'category_name'  => $category_id
-				) );
-				if ( $query->have_posts() ) :
+        // We have a different ordering method for reverse 1999 characters
+        if($category_id == 'Reverse: 1999') {
+          $query = new WP_Query( array(
+              'post_type'      => 'characters',
+              'posts_per_page' => -1,
+              'no_found_rows'  => true,
+              'category_name'  => $category_id,
+              'tax_query'      => array(
+                  'relation' => 'AND', // Specify that posts must have all specified tags
+                  array(
+                      'taxonomy' => 'post_tag',
+                      'field'    => 'slug',
+                      'terms'    => array( 'ur', 'ssr', 'sr', 'r' ),
+                  ),
+              ),
+              'orderby'        => 'terms_order', // Order by the order of tags specified
+              'order'          => 'DESC',
+          ) );
 
-						while ( $query->have_posts() ) : $query->the_post();
-
-              get_template_part('template-parts/character-nomargin');
-
-						endwhile; wp_reset_postdata();
-				endif; 
+          if ( $query->have_posts() ) :
+              while ( $query->have_posts() ) : $query->the_post();
+                  get_template_part('template-parts/character-nomargin');
+              endwhile;
+              wp_reset_postdata();
+          endif;
+        } else {
+          $query = new WP_Query( array(
+              'post_type'      => 'characters',
+              'orderby'        => 'title',
+              'order'          => 'ASC',
+              'posts_per_page' => -1,
+              'no_found_rows'  => true,
+              'category_name'  => $category_id
+          ) );
+          if ( $query->have_posts() ) :
+  
+              while ( $query->have_posts() ) : $query->the_post();
+  
+                get_template_part('template-parts/character-nomargin');
+  
+              endwhile; wp_reset_postdata();
+          endif; 
+        }
 			?>
 		</div>
 	</div>
