cd8a9d7d9d908b69b9fba61e681ba881048962d6
Sefhi <echo.mascarell@gmail.com>
2024-02-27 16:02:24 +0100

    added latest character reviews to guides pages

--- page-guides.php
+++ page-guides.php
@@ -215,6 +215,24 @@ if($category_id == 'Nikke') {
 		
 		<?php
 			$query = new WP_Query( array(
+					'post_type'      => 'characters',
+					'posts_per_page' => 6,
+					'no_found_rows'  => true,
+					'category_name'  => $category_id
+			) );
+			if ( $query->have_posts() ) :
+					?> <h2 class="guides-title">Latest character reviews ➜</h2><div class="guides"> <?php
+					while ( $query->have_posts() ) : $query->the_post();
+
+							get_template_part('template-parts/guide');
+
+					endwhile; wp_reset_postdata();
+					?> </div> <?php
+			endif; 
+		?>
+		
+		<?php
+			$query = new WP_Query( array(
 					'post_type'      => 'guides',
 					'posts_per_page' => -1,
 					'no_found_rows'  => true,
